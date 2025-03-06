1438e661c91b420def6b95f1922ad5543ea73f23
Damaroo <levkichigin@yandex.ru>
2025-03-06 16:46:40 +0300

    fix: api

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL_API_FTOYD = 'https://app.ftoydsd.com'
+export const BASE_URL_API_FTOYD = 'https://app.ftoyd.com'
 export const WSS_URL_API_FTOYD = 'wss://app.ftoyd.com/fronttemp-service/ws'
